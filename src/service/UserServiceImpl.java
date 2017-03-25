7c0a75244f998720aa178b622e297fcae4e2208f
andrei lorin <andreilorin@gmail.com>
2017-03-25 21:44:31 +0000

    service package and classes

---
+++ src/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import dao.UserDAO;
+import entity.User;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserDAO userDAO;
+	
+	@Override
+	@Transactional
+	public List<User> getUsers() {
+		return userDAO.getUsers();
+	}
+
+	@Override
+	@Transactional
+	public void saveUser(User user) {
+		
+		userDAO.saveUser(user);
+	}
+
+	@Override
+	@Transactional
+	public User getUser(int id) {
+
+		return userDAO.getUser(id);
+	}
+
+}
